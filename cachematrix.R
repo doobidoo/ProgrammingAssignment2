b892a9d56973df36ea6c53a0680612f2d4e65a87
Henry Krupp <henry.krupp@gmail.com>
2014-07-23 14:08:54 +0900

    some modifications

--- cachematrix.R
+++ cachematrix.R
@@ -1,54 +1,54 @@
 ## SOurce: https://class.coursera.org/rprog-005/human_grading/view/courses/972576/assessments/3/submissions
-## Modified sample functions makevector() and cachemean() to compute and cache the inversion of a matrix.
+## Recycle sample functions makevector() and cachemean() to compute and cache the inversion of a matrix.
+## Basically just renaming makevector() into makeCacheMatrix(), renaming the function cachemean() into cacheSolve(), and modifying it to use solve() instead of mean() on the input.
+## For better understanding renamed and commented everything accordingly.
 ##
-## How to use:
+## Example use:
 ##
-## 1. > my.matrix <- matrix(rnorm(9), nrow = 3)            # create or assign an invertible matrix to the variable name of your choice
+## 1. > my.matrix <- matrix(rnorm(10000), nrow = 100)      # create or assign an invertible matrix to the variable name of your choice
+## or > cache.my.matrix<- makeCacheMatrix(matrix(rnorm(10000), nrow = 100) ) # and skip step 2.
 ## 2. > cache.my.matrix <- makeCacheMatrix(my.matrix)      # create a list containing all the objetcs needed to store the cached matrix
 ## opt.  > cache.my.matrix$get()                           # optionally check if my.matrix was stored and can be accessed by the method get()
-## 3. > cacheSolve(my.matrix)                              # compute and return the inverse of my.matrix
-## opt.  > cacheSolve(my.matrix)                           # optionally Call function cachesolve() again to see that it returns the cached inverted matrix
+## 3. > cacheSolve(cache.my.matrix)                        # compute and return the inverse of my.matrix
+## opt.  > cacheSolve(cache.my.matrix)                     # optionally Call function cachesolve() again to see that it returns the cached inverted matrix
 
 
-
-# makeCacheMatrix: returns a list of functions to:
-# 1. Set the value of a matrix
-# 2. Get the value of a matrix
-# 3. Set the value of the inverted matrix
-# 4. Get the value of the inverted matrix
-
+## function makeCacheMatrix() returns a list of functions to:
+## 1. Set the value of a matrix
+## 2. Get the value of a matrix
+## 3. Set the value of the inverted matrix
+## 4. Get the value of the inverted matrix
 makeCacheMatrix <- function(x = matrix()) {
   # assign NULL to variable matrix
   matrix <- NULL
   #declare set function for the purpose to change the matrix stored in an instance of makeCacheMAtrix()
-  set <- function(y) {
-    x <<- y
-    matrix <<- NULL
+  f.set <- function(y) {
+    x <<- y # check if x exists and redefine its value
+    matrix <<- NULL # check if matrix exists and redefine its value
   }
   #declare get function to return x
-  get <- function() x
+  f.get <- function() x
   
   #store the setinversion function to store the inversion of x
-  setinversion <- function(inverse) matrix <<- inverse
-  #store the getinversion function to retrun the inversion of x
-  getinversion <- function() matrix
-  # the list that actually caches the stuff
-  list(set = set, get = get,
-       setinversion = setinversion,
-       getinversion = getinversion)
-
+  f.setinversion <- function(inverse) matrix <<- inverse
+  #store the getinversion function to return the inversion of x
+  f.getinversion <- function() matrix
+  # the list that actually caches the objects
+  list(set = f.set, get = f.get,
+       setinversion = f.setinversion,
+       getinversion = f.getinversion)
 }
 
 
-# cacheSolve() Computes the inverse of the matrix in case
-# it is not cached yet and returns the inversion
-
+## function cacheSolve() Computes the inverse of the matrix in case
+## it is not cached yet and returns the inversion
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     inverse <- x$getinversion() #x$getinversion() looks if x already has the cached inversion
     if(!is.null(inverse)) {
       # let us know if the inversion was returned from cache
       message("getting cached data")
+	  # in this case just return the cached data and end the function
       return(inverse)
     }
     ## read the matrix out of x's list  
